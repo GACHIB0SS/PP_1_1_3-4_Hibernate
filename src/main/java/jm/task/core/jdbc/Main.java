aa150c0cd7fd365475a0d72f09a12660af48948c
GACHIB0SS <der.confederate@gmail.com>
2022-07-16 23:51:53 +0500

    Долго пукал и мяукал

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Уй", "Простой", (byte) 44);
+        userService.saveUser("Лев", "Толстой", (byte) 94);
+        userService.saveUser("Александр", "Пистолетов", (byte) 55);
+        userService.saveUser("Джо", "Байден", (byte) 88);
+        System.out.println(userService.getAllUsers().toString());
+
+        userService.removeUserById(2);
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,87 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        String query = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR(45), lastName VARCHAR(45), age TINYINT(100));";
+        try {
+            Util.connection().createStatement().execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Usertable created");
     }
 
     public void dropUsersTable() {
-
+        String query = "DROP TABLE IF EXISTS users;";
+        try {
+            Util.connection().createStatement().execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Usertable deleted");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String query = "INSERT INTO users(name, lastName, age) VALUES('" + name + "', '" + lastName + "', '" + age + "');";
+        try {
+            Util.connection().createStatement().execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("User " + name + "_" + lastName + " added in database");
     }
 
     public void removeUserById(long id) {
-
+        String query = "DELETE FROM users WHERE id = " + id + ";";
+        try {
+            Util.connection().createStatement().execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("User id " + id + " deleted");
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = new ArrayList<>();
+        String query = "SELECT * FROM users;";
+        ResultSet res = null;
+        try {
+            res = Util.connection().createStatement().executeQuery(query);
+            while (res.next()) {
+                User user = new User();
+                user.setId(res.getLong(1));
+                user.setName(res.getString(2));
+                user.setLastName(res.getString(3));
+                user.setAge(res.getByte(4));
+                allUsers.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Userlist created");
+        return allUsers;
     }
 
     public void cleanUsersTable() {
-
+        String query = "DELETE FROM users;";
+        try {
+            Util.connection().createStatement().execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Usertable clear");
     }
 }
